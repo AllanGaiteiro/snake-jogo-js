7676749e114714aed97d09aaf94ff2ed4b0c3daa
Allan Gaiteiro <64567248+AllanGaiteiro@users.noreply.github.com>
2021-04-18 10:49:49 -0300

    create controler game

--- script/script.js
+++ script/script.js
@@ -1,5 +1,7 @@
 const canvas = document.getElementById('snake-js');
-const context = canvas.getContext('2d')
+const context = canvas.getContext('2d');
+let status = false;
+var frames;
 let box = 32;
 let snake = [];
 snake[0] = {
@@ -22,27 +24,52 @@ const createSnake = () => {
 }
 
 const initGame = () => {
+    document.addEventListener('keydown', controlGame);
     createBG();
     createSnake();
-
-    let snakeX = snake[0].x;
-    let snakeY = snake[0].y;
-
-    if (direction == 'right') snakeX += box;
-    if (direction == 'left') snakeX -= box;
-    if (direction == 'up') snakeY -= box;
-    if (direction == 'down') snakeY += box;
-
-    // remove 
-    snake.pop();
-    
-    // head snake
-    snake.unshift({x:snakeX,y:snakeY});
-
 }
 
+const controlGame = (event) => {
+    if ([37, 65].includes(event.keyCode) && direction !== 'right') direction = 'left';
+    if ([38, 87].includes(event.keyCode) && direction !== 'down') direction = 'up';
+    if ([39, 68].includes(event.keyCode) && direction !== 'left') direction = 'right';
+    if ([40, 83].includes(event.keyCode) && direction !== 'up') direction = 'down';
 
-setInterval(() => {
-    initGame();
-}, 100);
+    if ([32].includes(event.keyCode)){
+        if (status) {
+            clearInterval(frames)
+        } else {
+            framesGame();
+        }
+        status = !status;
+    }
+}
 
+const framesGame = () => {
+    frames = setInterval(() => {
+        if(snake[0].x > 16 * box && direction === 'right')snake[0].x = 0 ;
+        if(snake[0].x < 0 * box && direction === 'left')snake[0].x = 16 * box;
+        if(snake[0].y < 0 * box && direction === 'up')snake[0].y = 16 * box;
+        if(snake[0].y > 16 * box && direction === 'down')snake[0].y = 0;
+        
+        createBG();
+        createSnake();
+
+        let snakeX = snake[0].x;
+        let snakeY = snake[0].y;
+
+        if (direction === 'right') snakeX += box / 3;
+        if (direction === 'left') snakeX -= box / 3;
+        if (direction === 'up') snakeY -= box / 3;
+        if (direction === 'down') snakeY += box / 3;
+
+        // remove 
+        snake.pop();
+
+        // head snake
+        snake.unshift({ x: snakeX, y: snakeY });
+
+    }, 100);
+}
+
+initGame();
