edc2304654c2053cb1006023175b9f39deccbfe0
Allan Gaiteiro <64567248+AllanGaiteiro@users.noreply.github.com>
2021-04-17 22:06:52 -0300

    create snake

--- script/script.js
+++ script/script.js
@@ -1,12 +1,24 @@
 const canvas = document.getElementById('snake-js');
 const context = canvas.getContext('2d')
 let box = 32;
-
+let snake = [];
+snake[0] = {
+    x: 8 * box,
+    y: 8 * box,
+}
 // create back ground
-const criarBG = () => {
+const createBG = () => {
     context.fillStyle = "lightgreen";
     context.fillRect(0, 0, 16 * box, 16 * box);
 }
+// create snake
+const createSnake = () => {
+    for (const s of snake) {
+        context.fillStyle = "green";
+        context.fillRect(s.x, s.y,box,box);
+    }
+}
 
 
-criarBG();
+createBG();
+createSnake();
