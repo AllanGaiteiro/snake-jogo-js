170e2098037692db6dd0a3f8821772ae2ea8bcc7
Allan Gaiteiro <64567248+AllanGaiteiro@users.noreply.github.com>
2021-04-18 17:04:40 -0300

    base game snake

--- script/script.js
+++ script/script.js
@@ -1,6 +1,7 @@
 const canvas = document.getElementById('snake-js');
 const context = canvas.getContext('2d');
-let status = false;
+let menu = false;
+let score = 0;
 var frames;
 let box = 32;
 let snake = [];
@@ -23,45 +24,33 @@ const randonPosition = () => {
     } while (!!snake.find(s => s.x === random || s.y === random));
     return random;
 }
-/*
-const colid = (sX, sY, food) => {
-    let colid = false;
 
-    let snakeX = sX + (box / 2);
-    let snakeY = sY + (box / 2);
-    let foodX = food.x + (box / 2);
-    let foodY = food.y + (box / 2);
-
-    colidX = Math.abs(snakeX - foodX);
-    colidY = Math.abs(snakeY - foodY);
-
-    if (colidY < box && colidX < box) colid = true;
-
-    return colid;
-}
-*/
 const controlGame = (event) => {
     if ([37, 65].includes(event.keyCode) && direction !== 'right') direction = 'left';
     if ([38, 87].includes(event.keyCode) && direction !== 'down') direction = 'up';
     if ([39, 68].includes(event.keyCode) && direction !== 'left') direction = 'right';
     if ([40, 83].includes(event.keyCode) && direction !== 'up') direction = 'down';
 
-    if ([32].includes(event.keyCode)) {
-        if (status) {
-            clearInterval(frames)
-        } else {
-            framesGame();
+
+        if ([32].includes(event.keyCode)) {
+            if (!menu) {
+                clearInterval(frames)
+                menuGame()
+            }
+            menu = !menu;
         }
-        status = !status;
-    }
 }
 
 const framesGame = () => {
     frames = setInterval(() => {
-        if (snake[0].x > 16 * box && direction === 'right') snake[0].x = 0;
-        if (snake[0].x < 0 * box && direction === 'left') snake[0].x = 16 * box;
-        if (snake[0].y < 0 * box && direction === 'up') snake[0].y = 16 * box;
-        if (snake[0].y > 16 * box && direction === 'down') snake[0].y = 0;
+        if (snake.slice(1).find(s => s.x === snake[0].x && s.y === snake[0].y)) {
+            clearInterval(frames)
+        }
+
+        if (snake[0].x > 15 * box && direction === 'right') snake[0].x = 0;
+        if (snake[0].x < 0 * box && direction === 'left') snake[0].x = 15 * box;
+        if (snake[0].y < 0 * box && direction === 'up') snake[0].y = 15 * box;
+        if (snake[0].y > 15 * box && direction === 'down') snake[0].y = 0;
 
         createBG();
         createFood();
@@ -71,18 +60,18 @@ const framesGame = () => {
         let snakeY = snake[0].y;
 
         if (direction === 'right') snakeX += box;
-        if (direction === 'left') snakeX -= box ;
-        if (direction === 'up') snakeY -= box ;
+        if (direction === 'left') snakeX -= box;
+        if (direction === 'up') snakeY -= box;
         if (direction === 'down') snakeY += box;
 
         // colid food
-        if (snakeX != food.x || snakeY != food.y /*colid(snakeX, snakeY, food)*/) {
+        if (snakeX != food.x || snakeY != food.y) {
             snake.pop();
         } else {
             food.x = randonPosition();
             food.y = randonPosition();
+            score++;
         }
-
 
         // head snake
         snake.unshift({ x: snakeX, y: snakeY });
@@ -109,13 +98,22 @@ const createFood = () => {
 }
 
 const initGame = () => {
-    document.addEventListener('keydown', controlGame);
+    framesGame()
     createBG();
     createFood();
     createSnake();
-
 }
 
+const menuGame = () => {
+    menu = true;
+    $(document).ready(function () {
+        $('#modal').modal('show');
+    })
+}
+document.addEventListener('keydown', controlGame);
+createBG();
+menuGame();
 
 
-initGame();
+
+
